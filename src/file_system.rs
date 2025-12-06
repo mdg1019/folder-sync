907595c21d10fc7e1c48148aa1fcac302370239c
Mark Goodwin <m0good04@gmail.com>
2025-12-06 07:56:50 -0500

    Added a method to decompose a Node.

--- src/file_system.rs
+++ src/file_system.rs
@@ -7,23 +7,26 @@ pub enum Node {
     File {
         name: String,
         path: PathBuf,
+        metadata: fs::Metadata,
     },
     Directory {
         name: String,
         path: PathBuf,
+        metadata: fs::Metadata,
         children: Vec<Node>,
     },
 }
 
 pub fn build_tree(path: PathBuf) -> std::io::Result<Node> {
-    let metadata = fs::metadata(&path)?;
     let name = path
         .file_name()
         .map(|s| s.to_string_lossy().into_owned())
         .unwrap_or_else(|| path.to_string_lossy().to_string());
 
+    let metadata = fs::metadata(&path)?;
+
     if metadata.is_file() {
-        Ok(Node::File { name, path })
+        Ok(Node::File { name, path, metadata })
     } else {
         let mut children = Vec::new();
 
@@ -32,7 +35,7 @@ pub fn build_tree(path: PathBuf) -> std::io::Result<Node> {
             children.push(build_tree(entry.path())?);
         }
 
-        Ok(Node::Directory { name, path, children })
+        Ok(Node::Directory { name, path, metadata,children })
     }
 }
 
@@ -54,6 +57,13 @@ pub fn create_destination_dir_if_not_exists(destination_path: &PathBuf) -> Resul
     }
 
     Ok(true)
+}
+
+pub fn get_node_info(node: &Node) -> (&str, &String, &PathBuf, &fs::Metadata, Option<&Vec<Node>>) {
+    match node {
+        Node::File { name, path, metadata } => ("File", name, path, metadata, None),
+        Node::Directory { name, path, metadata, children } => ("Directory", name, path, metadata, Some(children)),
+    }
 }
 
 // pub fn print_tree(node: &Node, indent: usize) {
--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (source_path, destination_path) = utils::parse_args(args)?;
 
     println!("Building file tree for source path...");
-    let tree = file_system::build_tree(*source_path.clone())?;
+    let src_tree = file_system::build_tree(*source_path.clone())?;
 
     let dir_exists = create_destination_dir_if_not_exists(&destination_path)?;
 
     if !dir_exists {
         return Ok(());
     }
+
+    let (file_type, name, path, metadata, children) = file_system::get_node_info(&src_tree);
+    println!("File Type: {}, Name: {}, Path: {}, Metadata: {:?}, Children: {:?}", file_type, name, path.display(), metadata, children);
 
     Ok(())
 }
